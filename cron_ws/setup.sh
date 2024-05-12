2e127909261739554d14f36845a75881dc287a44
Satoshi <k.3104is@gmail.com>
2024-05-12 20:53:56 +0900

    update cron backup using ssh

--- cron_ws/setup.sh
+++ cron_ws/setup.sh
@@ -8,7 +8,7 @@ mkdir -p ${CRON_DIR}
 cat << EOF > ${CRON_DIR}/backup.sh
 #!/bin/bash
 
-tar czvf ${CRON_BACKUP_DIR}/bak_${USER}_workspace.tar.gz ~/workspace/
+tar czvf - -C ~/ ${BACKUP_DIR_NAME} | ssh -p ${BACKUP_DST_PORT} ${BACKUP_DST_IP} "cat > ${BACKUP_DST_PATH}"
 EOF
 ln -s $(pwd)/crontab ${CRON_DIR}/crontab
 
--- export.sh
+++ export.sh
@@ -8,3 +8,7 @@ GIT_USER_EMAIL='k.3104is@gmail.com'
 
 # cron_ws
 CRON_BACKUP_DIR='/mnt/nas1'
+BACKUP_DST_IP='Satoshi@192.168.10.100'
+BACKUP_DST_PORT='33333'
+BACKUP_DIR_NAME='workspace'
+BACKUP_DST_PATH="/var/services/homes/Satoshi/tmp/bak_${BACKUP_DIR_NAME}.tar.gz"
