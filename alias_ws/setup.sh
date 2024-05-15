de7af6818ed74239a51773b792708d3e7e1aed72
Satoshi <k.3104is@gmail.com>
2024-05-16 01:35:39 +0900

    add alias_ws

---
+++ alias_ws/setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! $(cat ~/.bashrc | grep "# my_aliases") ]; then
+cat << EOF >> ~/.bashrc
+
+# my_aliases
+if [ -f ~/.bash_myaliases ]; then
+  . ~/.bash_aliases
+fi
+EOF
+echo added
+fi
+
+cp ./.bash_myaliases ~/.bash_myaliases
+echo copied
+
+. ~/.bash_myaliases
