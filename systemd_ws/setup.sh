37bca7cb29ee7b2a327f89da814964104f511464
Satoshi Kataoka <k.3104is@gmail.com>
2024-05-31 19:31:00 +0900

    update systemd_ws

--- systemd_ws/setup.sh
+++ systemd_ws/setup.sh
@@ -1,12 +1,28 @@
 #!/bin/bash
 
 INIT_NAME="my_init"
-SERVICE_DIR="/etc/systemd/system/"
+SERVICE_DIR="/etc/systemd/system"
+SCRIPT_DIR="/home/${USER}/workspace/setup_ws/systemd_ws"
 SERVICE_NAME="${INIT_NAME}.service"
 SCRIPT_NAME="${INIT_NAME}.sh"
 
+tee ./${SERVICE_NAME} << EOF 
+[Unit]
+Description = ${INIT_NAME}
+After=local-fs.target
+ConditionPathExists=${SCRIPT_DIR}
+
+[Service]
+ExecStart=${SCRIPT_DIR}/${SCRIPT_NAME}
+Restart=no
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
 sudo chmod 764 ./${SCRIPT_NAME}
-sudo ln -sf $(pwd)/${SERVICE_NAME} ${SERVICE_DIR}
+sudo ln -sf $(pwd)/${SERVICE_NAME} ${SERVICE_DIR}/
 
 sudo systemctl daemon-reload
 sudo systemctl enable ${SERVICE_NAME}
