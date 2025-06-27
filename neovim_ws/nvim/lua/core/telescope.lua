f7811b80db08218a995f4c086763f3d896d94df2
Satoshi Kataoka <k.3104is@gmail.com>
2025-06-27 20:26:29 +0900

    add telescope keymap

---
+++ neovim_ws/nvim/lua/core/telescope.lua
@@ -0,0 +1,15 @@
+local actions = require("telescope.actions")
+local action_state = require("telescope.actions.state")
+
+require("telescope").setup({
+  defaults = {
+    mappings = {
+      i = {
+        ["<M-v>"] = actions.select_vertical,   -- Alt+v（Mac: Option+v）
+        ["<M-s>"] = actions.select_horizontal, -- Alt+s（splitのs）
+        ["<M-t>"] = actions.select_tab,        -- Alt+t（タブ）
+        ["jj"] = function(bufnr) actions.close(bufnr) end,  -- ←ここが追加部分
+      }
+    }
+  }
+})
