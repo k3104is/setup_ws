d309017711a96e6c946127d949e514259757d8bf
Satoshi Kataoka <k.3104is@gmail.com>
2025-07-04 06:01:14 +0900

    add softtabstop

--- neovim_ws/nvim/lua/core/options.lua
+++ neovim_ws/nvim/lua/core/options.lua
@@ -3,6 +3,7 @@ vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
+vim.opt.softtabstop = 2
 vim.o.expandtab = true
 vim.o.smartindent = true
 vim.o.termguicolors = true
