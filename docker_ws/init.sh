8da624a8fe969eb176ac5e46a93bb570ba32479b
Satoshi <k.3104is@gmail.com>
2026-02-23 16:02:27 +0900

    add nvidia-container-toolkit to docker

--- docker_ws/init.sh
+++ docker_ws/init.sh
@@ -11,4 +11,13 @@ sudo apt-get -y install docker-compose-plugin
 sudo gpasswd -a ${USER} docker
 newgrp docker
 
-docker run hello-world
+# nvidia-container-toolkit
+if ! $(nvidia-smia > /dev/null 2>&1); then
+  echo "ng"
+  return
+fi
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+sudo apt-get update && sudo apt install -y nvidia-container-toolkit
